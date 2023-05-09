ccf6c978c98cf79dfa315ac8322a9fe2092e8181
NoahKeijzer <noah.ccristiaan@gmail.com>
2023-05-09 22:57:05 +0200

    Kinda works

---
+++ KillerSudokuSolver/KillerSudoku.cs
@@ -0,0 +1,19 @@
+﻿namespace KillerSudokuSolver;
+
+public class KillerSudoku
+{
+    
+    public KillerSudoku(int[,] killerSudoku)
+    {
+        Map(killerSudoku);
+    }
+    public List<Tuple<int, List<Cell>>> mappedPuzzle;
+
+    private void Map(int[,] killerSudoku)
+    {
+        foreach (var row in killerSudoku)
+        {
+            
+        }
+    }
+}
--- KillerSudokuSolver/SudokuSolver.cs
+++ KillerSudokuSolver/SudokuSolver.cs
@@ -1,4 +1,6 @@
-﻿namespace KillerSudokuSolver;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace KillerSudokuSolver;
 
 public class SudokuSolver
 {
@@ -28,7 +30,7 @@ public class SudokuSolver
         { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     };
 
-    private readonly int[,] _emptySudokuPuzzle 
+    private readonly int[,] _emptySudokuPuzzle =
     {
         { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -40,13 +42,6 @@ public class SudokuSolver
         { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
     };
-
-    private IEnumerable<int> GetColumnValues(Cell emptyCell)
-    {
-        return Enumerable.Range(0, 9)
-            .Select(x => _emptySudokuPuzzle[x, emptyCell.ColumnIndex])
-            .ToArray();
-    }
     private IEnumerable<Cell> GetColumnCells(Cell emptyCell)
     {
         return Enumerable.Range(0, 9)
@@ -58,13 +53,12 @@ public class SudokuSolver
     {
         return Enumerable.Range(0, 9)
             .Select(x => _emptySudokuPuzzle[emptyCell.RowIndex, x])
-            .ToArray();
+            .ToList();
     }
-    private IEnumerable<Cell> GetRowCells(Cell emptyCell)
+    private IEnumerable<int> GetColumnValues(Cell emptyCell)
     {
         return Enumerable.Range(0, 9)
-            .Select(columnIndex => new Cell(emptyCell.RowIndex, columnIndex,
-                _killerSudokuPuzzle[emptyCell.RowIndex, columnIndex]))
+            .Select(x => _emptySudokuPuzzle[x, emptyCell.ColumnIndex])
             .ToList();
     }
     private IEnumerable<int> GetGridValues(Cell emptyCell)
@@ -82,6 +76,40 @@ public class SudokuSolver
             }
         }
     }
+    private IEnumerable<int> GetRowTargetSumValues(Cell emptyCell)
+    {
+        return Enumerable.Range(0, 9)
+            .Select(x => _killerSudokuPuzzle[emptyCell.RowIndex, x])
+            .ToList();
+    }
+    private IEnumerable<int> GetColumnTargetSumValues(Cell emptyCell)
+    {
+        return Enumerable.Range(0, 9)
+            .Select(x => _killerSudokuPuzzle[x, emptyCell.ColumnIndex])
+            .ToList();
+    }
+    private IEnumerable<int> GetGridTargetSumValues(Cell emptyCell)
+    {
+        // Calculate the starting indexes of the 3x3 grid based on the given column and row indexes
+        var startColumnIndex = (emptyCell.ColumnIndex / 3) * 3;
+        var startRowIndex = (emptyCell.RowIndex / 3) * 3;
+
+        // Iterate through each cell in the 3x3 grid and yield its value
+        for (var i = startRowIndex; i < startRowIndex + 3; i++)
+        {
+            for (var j = startColumnIndex; j < startColumnIndex + 3; j++)
+            {
+                yield return _killerSudokuPuzzle[i, j];
+            }
+        }
+    }
+    private IEnumerable<Cell> GetRowCells(Cell emptyCell)
+    {
+        return Enumerable.Range(0, 9)
+            .Select(columnIndex => new Cell(emptyCell.RowIndex, columnIndex,
+                _killerSudokuPuzzle[emptyCell.RowIndex, columnIndex]))
+            .ToList();
+    }
     private bool IsInColumn(int valueToPut, Cell emptyCell)
     {
         return GetColumnValues(emptyCell).Contains(valueToPut);
@@ -90,126 +118,154 @@ public class SudokuSolver
     {
         return GetRowValues(emptyCell).Contains(valueToPut);
     }
-
     private bool IsInGrid(int valueToPut, Cell emptyCell)
     {
         return GetGridValues(emptyCell).Contains(valueToPut);
     }
-
-    private bool IsValid(int valueToPut, Cell emptyCell)
+    private bool IsNeededInRow(int valueToPut, Cell emptyCell)
     {
-        return !IsInColumn(valueToPut, emptyCell) && !IsInRow(valueToPut, emptyCell) &&
-               !IsInGrid(valueToPut, emptyCell) && !IsSurpassingTargetSum(valueToPut, emptyCell) &&
-               !IsNeededInRow(valueToPut, emptyCell);
-    }
+        var rowCells = FilterValuesInBetween(emptyCell.RowIndex, GetRowTargetSumValues(emptyCell).ToList()).ToList();
 
+        var relatedToEmptyCell = GetRelatedCells(emptyCell).ToList();
+        relatedToEmptyCell.Add(emptyCell);
+        relatedToEmptyCell.ForEach(c => rowCells.RemoveAll(rc => rc.RowIndex == c.RowIndex && rc.ColumnIndex == c.ColumnIndex));
+
+        return CheckIfNeeded(rowCells, emptyCell, valueToPut);
+    }
+    private bool IsNeededInColumn(int valueToPut, Cell emptyCell)
+    {
+        var colCells = FilterValuesInBetween(emptyCell.ColumnIndex, GetColumnTargetSumValues(emptyCell).ToList()).ToList();
+
+        var relatedToEmptyCell = GetRelatedCells(emptyCell).ToList();
+        relatedToEmptyCell.Add(emptyCell);
+        relatedToEmptyCell.ForEach(c => colCells.RemoveAll(rc => rc.RowIndex == c.RowIndex && rc.ColumnIndex == c.ColumnIndex));
+
+        return CheckIfNeeded(colCells, emptyCell, valueToPut);
+    }
+    private bool IsNeededInGrid(int valueToPut, Cell emptyCell)
+    {
+        var colCells = FilterValuesInBetween(emptyCell.ColumnIndex, GetGridTargetSumValues(emptyCell).ToList()).ToList();
+
+        var relatedToEmptyCell = GetRelatedCells(emptyCell).ToList();
+        relatedToEmptyCell.Add(emptyCell);
+        relatedToEmptyCell.ForEach(c => colCells.RemoveAll(rc => rc.RowIndex == c.RowIndex && rc.ColumnIndex == c.ColumnIndex));
+
+        return CheckIfNeeded(colCells, emptyCell, valueToPut);
+    }
     private bool IsSurpassingTargetSum(int valueToPut, Cell emptyCell)
     {
         var relatedCells = GetRelatedCells(emptyCell).ToList();
         relatedCells.Add(emptyCell);
-        return relatedCells.Sum(x => _emptySudokuPuzzle[x.RowIndex, x.ColumnIndex]) + valueToPut >= emptyCell.TargetSum;
+        var surpassesTarget = relatedCells.Sum(x => _emptySudokuPuzzle[x.RowIndex, x.ColumnIndex]) + valueToPut > emptyCell.TargetSum;
+        return surpassesTarget;
     }
-
-    private bool IsNeededInRow(int valueToPut, Cell emptyCell)
+    private bool IsReachingTargetSum(int valueToPut, Cell emptyCell)
     {
-        var rowCells = GetRowCells(emptyCell);
-        var allRelatedRowCells = new List<List<Cell>>();
-        var visited = new List<Cell>();
-        var possible = new Dictionary<List<Tuple<Cell, List<int>>>, bool>();
-
-        foreach (var cell in rowCells)
-        {
-            var listRelated = GetRelatedCells(cell).ToList();
-            var allowed = true;
-            foreach (var v in visited.Where(v => listRelated.Any(c => c.RowIndex == v.RowIndex && c.ColumnIndex == v.ColumnIndex)))
-            {
-                allowed = false;
-            }
-
-            if (!allowed) continue;
-            listRelated.Add(cell);
-            visited.Add(cell);
-            allRelatedRowCells.Add(listRelated);
-
-        }
-
-        foreach (var rowCell in allRelatedRowCells)
-        {
-            var possibleSums = GetPossibleSums(rowCell.First(c => c.RowIndex == emptyCell.RowIndex).TargetSum, rowCell.Count);
-            var sumPossibility = new List<Tuple<Cell, List<int>>>();
-            possible[sumPossibility] = false;
-
-            foreach (var c in possibleSums.Values.SelectMany(combination => combination))
-            {
-                sumPossibility.Add(new Tuple<Cell, List<int>>(rowCell.First(cell => cell.RowIndex == emptyCell.RowIndex), c));
-                if (!c.Contains(valueToPut))
-                    possible[sumPossibility] = true;
-            }
-        }
-        return false;
+        var relatedCells = GetRelatedCells(emptyCell).ToList();
+        var totalRelated = relatedCells.Count; 
+        var totalFilled = relatedCells.Count(cell => _emptySudokuPuzzle[cell.RowIndex, cell.ColumnIndex] != 0);
+        
+        if(totalRelated == totalFilled)
+            return relatedCells.Sum(x => _emptySudokuPuzzle[x.RowIndex, x.ColumnIndex]) + valueToPut == emptyCell.TargetSum;
+        return true;
     }
-    
-    private Dictionary<int, List<List<int>>> GetPossibleSums(int? target, int amountOfNumbers)
-    {
-        var result = new Dictionary<int, List<List<int>>>();
-
-        for (int i = 1; i <= target; i++)
-        {
-            result.Add(i, new List<List<int>>());
-        }
-
-        for (int i = 1; i <= amountOfNumbers; i++)
-        {
-            var combinations = GetCombinations(i, amountOfNumbers);
-            foreach (var combination in combinations)
-            {
-                var sum = combination.Sum();
-                if (sum <= target)
-                {
-                    result[sum].Add(combination);
-                }
-            }
-        }
-
-        return result;
-    }
-
-    private List<List<int>> GetCombinations(int count, int max)
-    {
-        if (count == 1)
-        {
-            return Enumerable.Range(1, max).Select(x => new List<int> { x }).ToList();
-        }
-
-        var result = new List<List<int>>();
-
-        for (var i = 1; i <= max - count + 1; i++)
-        {
-            var subCombinations = GetCombinations(count - 1, max - i);
-            foreach (var subCombination in subCombinations)
-            {
-                subCombination.Insert(0, i);
-                result.Add(subCombination);
-            }
-        }
-
-        return result;
-    }
-
     private bool IsNeededForAnotherCell(int valueToPut, Cell emptyCell)
     {
-        return false;
+        return IsNeededInRow(valueToPut, emptyCell) && IsNeededInColumn(valueToPut, emptyCell) && IsNeededInGrid(valueToPut, emptyCell);
     }
+    private bool IsInAnyPossibleCombination(int valueToPut, Cell emptyCell)
+    {
+        var relatedCells = GetRelatedCells(emptyCell).ToList();
+        relatedCells.Add(emptyCell);
+        var possibleCombinations = GetNumberCombinations(emptyCell.TargetSum, relatedCells.Count);
+        var isInAnyCombination = possibleCombinations.Any(c => c.Any(l => l.Equals(valueToPut)));
+        return isInAnyCombination;
+    }
+    private bool CheckIfNeeded(List<Cell> cells, Cell emptyCell, int valueToPut)
+    {
+        var rowCellCombination = new Dictionary<Cell, bool>();
+        
+        foreach (var t in cells)
+        {
+            var relatedCells = GetRelatedCells(t).ToList();
+            var possibleCombinations = GetNumberCombinations(t.TargetSum, relatedCells.Count + 1);
+            var canBeDoneWithout = possibleCombinations.ToList().Any(c => !c.Contains(valueToPut));
+            var anyCellOnAnotherRow = relatedCells.Any(c => c.RowIndex != emptyCell.RowIndex);
+            rowCellCombination.Add(t, canBeDoneWithout || anyCellOnAnotherRow);
+        }
+        var isNeededInRow = rowCellCombination.Values.Any(c => c.Equals(false));
+        return !isNeededInRow;
+    }
+    private static IEnumerable<Cell> FilterValuesInBetween(int rowIndex, List<int> cells)
+    {
+        var filteredCells = new List<Cell>();
+        for (var i = 0; i < cells.Count - 1; i++) {
+            if (cells[i] == cells[i + 1]) {
+                var hasDifferentValueInBetween = false;
+                for (var j = i + 1; j < cells.Count - 1; j++)
+                {
+                    if (cells[j] == cells[i]) continue;
+                    hasDifferentValueInBetween = true;
+                    break;
+                }
+                if (!hasDifferentValueInBetween) {
+                    filteredCells.Add(new Cell(rowIndex, i, cells[i]));
+                }
+            } else {
+                filteredCells.Add(new Cell(rowIndex, i, cells[i]));
+            }
+        }
 
+        var cell = cells[^1];
+        filteredCells.Add(new Cell(rowIndex, 8, cells[^1]));
+        return filteredCells;
+    }
+    private static IEnumerable<List<int>> GetNumberCombinations(int targetSum, int limit)
+    {
+       var numbers = Enumerable.Range(1, 9).ToList();
 
-    public IEnumerable<Cell> GetRelatedCells(Cell cell)
+        var allCombinations = new List<List<int>>();
+        for (var i = 1; i <= numbers.Count; i++)
+        {
+            allCombinations.AddRange(Combinations(numbers, limit));
+        }
+        
+        var validCombinations = allCombinations.Where(combo => combo.Sum() == targetSum)
+            .ToList();  
+
+        // Helper method to get combinations of a given length
+        List<List<int>> Combinations(IEnumerable<int> items, int length)
+        {
+            if (length == 1)
+            {
+                return items.Select(item => new List<int> { item }).ToList();
+            }
+
+            var result = new List<List<int>>();
+            var enumerable = items.ToList();
+            
+            for (var i = 0; i < enumerable.ToList().Count; i++)
+            {
+               var subItems = enumerable.Skip(i + 1).ToList();
+               var subCombinations = Combinations(subItems, length - 1);
+
+                foreach (var subCombination in subCombinations)
+                {
+                    subCombination.Insert(0, enumerable[i]);
+                    result.Add(subCombination);
+                }
+            }
+            return result;
+        }
+        return validCombinations;
+    }
+    private IEnumerable<Cell> GetRelatedCells(Cell cell)
     {
         var relatedCells = new List<Cell>();
         var visitedCells = new HashSet<Cell> { cell };
         GetRelatedCellsRecursive(cell, relatedCells, visitedCells);
         return relatedCells;
     }
-
     private void GetRelatedCellsRecursive(Cell cell, ICollection<Cell> relatedCells, ISet<Cell> visitedCells)
     {
         // Check cell above
@@ -280,7 +336,6 @@ public class SudokuSolver
             }
         }
     }
-
     private Cell? FindEmptyCell()
     {
         for (var row = 0; row < 9; row++)
@@ -294,10 +349,18 @@ public class SudokuSolver
 
         return null;
     }
-
+    private bool IsValid(int valueToPut, Cell emptyCell)
+    {
+        return !IsInColumn(valueToPut, emptyCell) &&
+               !IsInRow(valueToPut, emptyCell) &&
+               !IsInGrid(valueToPut, emptyCell) &&
+               !IsSurpassingTargetSum(valueToPut, emptyCell) &&
+               IsReachingTargetSum(valueToPut, emptyCell);
+    }
     public void PrintSudokuGrid()
     {
-        for (int row = 0; row < 9; row++)
+        Console.SetCursorPosition(0, 0);
+        for (var row = 0; row < 9; row++)
         {
             if (row % 3 == 0 && row != 0)
             {
@@ -336,6 +399,7 @@ public class SudokuSolver
         for (var i = 1; i < 10; i++)
         {
             if (!IsValid(i, emptyCell)) continue;
+            PrintSudokuGrid();
             _emptySudokuPuzzle[emptyCell.RowIndex, emptyCell.ColumnIndex] = i;
 
             if (Solve())
